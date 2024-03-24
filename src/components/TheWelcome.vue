60f2304a0f06214d4308ad9ed1ca839a00cb48dd
AxsGro <ale.silva.gro@outlook.es>
2024-03-24 11:19:32 -0600

    welcome no funciona, gitignore comentado

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,88 +1,210 @@
-<script setup>
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+<template>
+  <div class="pantalla-principal">
+    <img class="image" alt="Image" src="image-2.png" />
+    <img class="img" alt="Image" src="image-1.png" />
+    <header class="header">
+      <div class="text-wrapper">K’ peliculon</div>
+      <img class="icon-user-avatar" alt="Icon user avatar" src="icon-user-avatar.png" />
+      <img class="icon-menu" alt="Icon menu" src="icon-menu.png" />
+      <div class="div">Películas</div>
+      <div class="text-wrapper-2">Inicio</div>
+      <div class="text-wrapper-3">Géneros</div>
+      <img class="icon-chevron-down" alt="Icon chevron down" src="icon-chevron-down.png" />
+      <img class="icon-chevron-down-2" alt="Icon chevron down" src="image.png" />
+    </header>
+    <img class="image-2" alt="Image" src="image-3.png" />
+    <img class="image-3" alt="Image" src="image-4.png" />
+    <img class="image-4" alt="Image" src="image-5.png" />
+    <img class="image-5" alt="Image" src="image-6.png" />
+    <img class="image-6" alt="Image" src="image-9.png" />
+    <img class="image-7" alt="Image" src="image-11.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PantallaPrincipal",
+};
 </script>
 
-<template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
+<style>
+.pantalla-principal {
+  background-color: #000000;
+  height: 1024px;
+  position: relative;
+  width: 1440px;
+}
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
+.pantalla-principal .image {
+  height: 230px;
+  left: 39px;
+  object-fit: cover;
+  position: absolute;
+  top: 582px;
+  width: 151px;
+}
 
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
+.pantalla-principal .img {
+  height: 225px;
+  left: 39px;
+  object-fit: cover;
+  position: absolute;
+  top: 317px;
+  width: 151px;
+}
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
+.pantalla-principal .header {
+  background-color: transparent;
+  border: 1px solid;
+  border-color: transparent;
+  border-radius: 2px;
+  height: 114px;
+  left: 0;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  width: 1440px;
+}
 
-    <br />
+.pantalla-principal .text-wrapper {
+  color: #ffffff;
+  font-family: "Oswald-Regular", Helvetica;
+  font-size: 48px;
+  font-weight: 400;
+  height: 71px;
+  left: 39px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 21px;
+}
 
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
+.pantalla-principal .icon-user-avatar {
+  height: 60px;
+  left: 1246px;
+  position: absolute;
+  top: 27px;
+  width: 60px;
+}
 
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
+.pantalla-principal .icon-menu {
+  height: 38px;
+  left: 1345px;
+  position: absolute;
+  top: 38px;
+  width: 50px;
+}
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
+.pantalla-principal .div {
+  color: #ffffff;
+  font-family: "Oswald-Regular", Helvetica;
+  font-size: 24px;
+  font-weight: 400;
+  height: 36px;
+  left: 479px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 38px;
+}
 
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
+.pantalla-principal .text-wrapper-2 {
+  color: #ffffff;
+  font-family: "Oswald-Regular", Helvetica;
+  font-size: 24px;
+  font-weight: 400;
+  height: 36px;
+  left: 355px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 38px;
+}
 
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
+.pantalla-principal .text-wrapper-3 {
+  color: #ffffff;
+  font-family: "Oswald-Regular", Helvetica;
+  font-size: 24px;
+  font-weight: 400;
+  height: 36px;
+  left: 633px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 38px;
+}
 
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
+.pantalla-principal .icon-chevron-down {
+  height: 7px;
+  left: 566px;
+  position: absolute;
+  top: 57px;
+  width: 12px;
+}
 
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
-</template>
+.pantalla-principal .icon-chevron-down-2 {
+  height: 7px;
+  left: 713px;
+  position: absolute;
+  top: 57px;
+  width: 12px;
+}
+
+.pantalla-principal .image-2 {
+  height: 225px;
+  left: 228px;
+  object-fit: cover;
+  position: absolute;
+  top: 317px;
+  width: 151px;
+}
+
+.pantalla-principal .image-3 {
+  height: 230px;
+  left: 228px;
+  object-fit: cover;
+  position: absolute;
+  top: 582px;
+  width: 151px;
+}
+
+.pantalla-principal .image-4 {
+  height: 225px;
+  left: 417px;
+  object-fit: cover;
+  position: absolute;
+  top: 317px;
+  width: 155px;
+}
+
+.pantalla-principal .image-5 {
+  height: 230px;
+  left: 417px;
+  object-fit: cover;
+  position: absolute;
+  top: 582px;
+  width: 155px;
+}
+
+.pantalla-principal .image-6 {
+  height: 224px;
+  left: 789px;
+  object-fit: cover;
+  position: absolute;
+  top: 318px;
+  width: 159px;
+}
+
+.pantalla-principal .image-7 {
+  height: 224px;
+  left: 986px;
+  object-fit: cover;
+  position: absolute;
+  top: 318px;
+  width: 159px;
+}
+</style>
